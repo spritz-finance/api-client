dda73b7014ebb42d4956bd8d6cdebfcf65ff071f
Eliyahu Gusovsky <eliyahud@gmail.com>
2023-05-09 09:12:54 +0300

    first

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import { Environment } from './env'
 
 type Config = {
+    baseEndpoint: string
     graphEndpoint: string
 }
 
@@ -8,9 +9,11 @@ type ApiConfig = Record<Environment, Config>
 
 export const config: ApiConfig = {
     staging: {
+        baseEndpoint: 'https://api-staging.spritz.finance',
         graphEndpoint: 'https://api-staging.spritz.finance/graphql',
     },
     production: {
+        baseEndpoint: 'https://api.spritz.finance',
         graphEndpoint: 'https://api.spritz.finance/graphql',
     },
 }
