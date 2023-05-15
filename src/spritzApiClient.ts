31d97f4752953fc320699fc0ebdd5c3a0fb92f93
Eliyahu Gusovsky <eliyahud@gmail.com>
2023-05-15 17:14:18 +0300

    set api key

--- src/spritzApiClient.ts
+++ src/spritzApiClient.ts
@@ -13,17 +13,29 @@ export class SpritzApiClient {
     public paymentRequest: PaymentRequestService
     public payment: PaymentService
 
-    public static initialize({ environment, apiKey, integrationKey }: ClientParams) {
-        const client = new SpritzApiClient()
-        client.init(environment, apiKey, integrationKey)
+    constructor(
+        private apiKey: string,
+        private integrationKey: string,
+        private environment: Environment
+    ) {}
+
+    public static initialize({ environment, apiKey = '', integrationKey = '' }: ClientParams) {
+        const client = new SpritzApiClient(apiKey, integrationKey!, environment)
+        client.init()
         return client
     }
 
-    private init(environment: Environment, apiKey: string, integrationKey?: string) {
-        this.client = new GraphClient(environment, apiKey, integrationKey)
+    private init() {
+        this.client = new GraphClient(this.environment, this.apiKey, this.integrationKey)
         this.user = new UserService(this.client)
         this.bankAccount = new BankAccountService(this.client)
         this.paymentRequest = new PaymentRequestService(this.client)
         this.payment = new PaymentService(this.client)
+    }
+
+    setApiKey(_apiKey: string) {
+        this.apiKey = _apiKey
+        this.init()
+        return this
     }
 }
