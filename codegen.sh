7cd353562a2b35452f93ef6892161a11fc40b42d
ljd-davies <ljd.davies@gmail.com>
2025-07-16 13:36:49 +0200

    Switch GraphQL introspection to staging API with authentication

--- codegen.sh
+++ codegen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 find . -type d -name "__types__" -exec rm -r {} + &&
-yarn apollo service:download --endpoint=http://localhost:4000 graphql-schema.json &&
+yarn apollo service:download --header="X-API-KEY: $INTROSPECTION_KEY" --endpoint=https://api-staging.spritz.finance/router/graph-introspection  graphql-schema.json &&
 yarn apollo codegen:generate __types__  --globalTypesFile=src/types/globalTypes.ts --localSchemaFile=graphql-schema.json --target=typescript --tagName=gql &&
 yarn prettier --write '**/__types__/*.ts' --loglevel=silent && yarn rimraf graphql-schema.json 
 . export_types.sh
